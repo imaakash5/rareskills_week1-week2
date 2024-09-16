5920ba5817e3d8308eaee7e3e8da8cab4c0c77e3
imaakash5 <44193567+imaakash5@users.noreply.github.com>
2024-09-16 18:25:14 +0530

    week2

---
+++ contracts/NFT.sol
@@ -0,0 +1,16 @@
+//SPDX-Identifier : Unlicensed
+pragma solidity 0.8.23;
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721,Ownable{
+    constructor()ERC721("FirstCollection","NFT")Ownable(msg.sender){}
+    uint256 private constant maxSupply=20; 
+    uint256 public totalSupply=0;
+
+    function mint()external onlyOwner{
+        require(totalSupply<maxSupply);
+        _mint(msg.sender,totalSupply);
+        totalSupply++;
+    }
+}
---
+++ tests/NFT.t.sol
@@ -0,0 +1,23 @@
+//SPDX - Identified : Unlicensed
+pragma solidity 0.8.23;
+import {NFT} from "../contracts/NFT.sol";
+import {console,Test} from "forge-std/Test.sol";
+
+contract testNFT is Test{
+    NFT private token;
+    address private admin;
+    function setUp() external {
+        admin = vm.addr(12345);
+        vm.prank(admin);
+        //deployer
+        token = new NFT();
+    }
+    function testMint() external{
+        //owner minting the tokens
+        vm.prank(admin);
+        token.mint();
+        vm.expectRevert();
+        token.mint();
+        assertEq(token.totalSupply(),1);
+    }
+}
